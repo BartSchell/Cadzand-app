2bf8e96383b57ea8199ec0d40ac77622168bea0c
BartSchell <bartschellekens@outlook.com>
2026-06-28 21:58:12 +0200

    Update Firebase configuration with real credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "REPLACE_ME",
-  authDomain: "REPLACE_ME.firebaseapp.com",
-  projectId: "REPLACE_ME",
-  storageBucket: "REPLACE_ME.appspot.com",
-  messagingSenderId: "REPLACE_ME",
-  appId: "REPLACE_ME",
+  apiKey: "AIzaSyA_oMEnIHNcCN1yJWYmE0v8rnshwsN5D-4",
+  authDomain: "cadzand-huisje.firebaseapp.com",
+  projectId: "cadzand-huisje",
+  storageBucket: "cadzand-huisje.firebasestorage.app",
+  messagingSenderId: "534633852485",
+  appId: "1:534633852485:web:3daf74831058a61df63d2c",
 };
 
 const app = initializeApp(firebaseConfig);
